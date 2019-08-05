26c0ce049c721b018ff48457c7e0c021a91d94cd
NghiNV <nghinv@luci.vn>
2019-08-05 14:16:04 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+* Created by nghinv on Mon Aug 05 2019
+* Copyright (c) 2019 nghinv@lumi.biz
+*/
+
+const add = (a, b) => {
+  return a + b
+}
+
+let tong = add(10, 7)
+console.log('tong', tong)
